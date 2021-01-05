813704e0c288b9276e955e2aaa0bad503906c698
Damian Romanowski <romfir22@gmail.com>
2021-01-05 04:19:25 +0100

    Add project files.

---
+++ Chat/Server/Hubs/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using Chat.Shared;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace Chat.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+        //todo mozna dodac jakas zabawe z datami
+        //todo nie powinnismy w sumie uzywac all tylko Other, ale co jak jest zalgoowany na dwoch kompach? ALl zostaje
+        public async Task SendMessage(Message message)
+            => await Clients.All.SendAsync("ReceiveMessage", message).ConfigureAwait(false);
+
+        //todo usuniecie tez z bazy jakbysmy jakas mieli
+        //todo 2 walidacja usera ofc jakby jakas byla
+        public async Task DeleteMessage(Guid messageId)
+            => await Clients.All.SendAsync("DeleteMessage", messageId).ConfigureAwait(false);
+
+        public async Task UserWriting(string userName)
+            => await Clients.Others.SendAsync("UserIsWriting", userName).ConfigureAwait(false);
+
+        public override Task OnConnectedAsync() => base.OnConnectedAsync();
+
+        public override Task OnDisconnectedAsync(Exception exception) => base.OnDisconnectedAsync(exception);
+    }
+}
---
+++ Chat/Shared/Message.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Chat.Shared
+{
+    public class Message
+    {
+        public Message()
+        { }
+
+        public Message(string user, string text, DateTime timeStamp)
+        {
+            User = user;
+            Text = text;
+            TimeStamp = timeStamp;
+        }
+
+        public Guid Id { get; set; }
+
+        [Required, MaxLength(50)]
+        public string User { get; set; }
+
+        [Required, MaxLength(50), MinLength(1)]
+        public string Text { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+    }
+}
